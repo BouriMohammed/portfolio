919b2ebef885ec7c1f471bd9db63cf5e25664843
Mohammed Bouri <54592816+BouriMohammed@users.noreply.github.com>
2021-10-14 17:06:09 +0100

    Updates

---
+++ precache-manifest.4e14ecf4b95d42e8a0018f538c5d2ae6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d29e6c062b6be7bd8766a11791efd1f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e9952ee225a25db27798",
+    "url": "/portfolio/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/portfolio/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9952ee225a25db27798",
+    "url": "/portfolio/static/js/main.815263a7.chunk.js"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/portfolio/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/portfolio/static/media/resume.9d3da0e2.pdf"
+  }
+]);
